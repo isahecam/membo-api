5956d5c10a6280461753b775d75853ee8858867d
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-09-25 20:53:18 -0600

    feat: create Membership model with relationships to category, payment method, and user

---
+++ app/Models/Membership.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+    protected $table = 'memberships';
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        "deleted_at"
+    ];
+
+    protected $fillable = [
+        'concept',
+        'description',
+        'amount',
+        'currency',
+        "frequency",
+        "payment_date",
+        "next_payment_date",
+        "category_id",
+        "payment_method_id",
+        "user_id",
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class, 'payment_method_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
