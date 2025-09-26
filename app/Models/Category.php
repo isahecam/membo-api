2c99fdb979ad0a79a2cc5b22f0c7ea9695a6b4d0
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-09-25 20:53:10 -0600

    feat: create Category model with relationships to memberships

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function memberships()
+    {
+        return $this->hasMany(Membership::class, 'category_id');
+    }
+}
