00d05ee53598093a9833532d70130554153228eb
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-09-26 11:25:56 -0600

    docs: add API route documentation for membership management

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\MembershipController;
 use Illuminate\Support\Facades\Route;
 
+/*
+* Resource API routes for membership management
+*/
+
 Route::apiResource('memberships', MembershipController::class);
