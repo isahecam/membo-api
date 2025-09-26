205bdad4988649f53b4ac15d2932ea4626322ae9
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-09-26 11:11:59 -0600

    feat: add seed data for categories, payment methods and memberships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Membership;
+use App\Models\PaymentMethod;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->create([
+      'name' => 'Test User',
+      'email' => 'test@example.com',
+    ]);
+
+    Category::factory(5)->create();
+    PaymentMethod::factory(5)->create();
+
+    User::factory(5)->create()->each(function ($user) {
+      Membership::factory(10)->create([
+        'category_id' => Category::inRandomOrder()->first()->id,
+        'payment_method_id' => PaymentMethod::inRandomOrder()->first()->id,
+        'user_id' => $user->id,
+      ]);
+    });
+  }
 }
